1cb7856c921fa728721a5057291df4250e607de0
renanmuniz <renan.merlo@paygo.com.br>
2022-10-19 14:54:56 -0300

    Conditionals

---
+++ kotlin_basics/src/main/kotlin/Conditionals.kt
@@ -0,0 +1,39 @@
+fun main() {
+    var calories = 2500
+    if(calories > 2000) {
+        println("You've cosnsumed all your calories for the day")
+    }
+
+    calories = 1900
+    if(calories > 2000) {
+        println("You've consumed all your calories for the day")
+    } else {
+        println("You've not consumed all your calories for the day")
+    }
+
+
+    val age = 9
+    if(age > 10)
+        println("is greater than 10")
+    else
+        println("is less than 10")
+    println("this is out of the if scope")
+
+
+    var bool1: Boolean = true
+    var bool2: Boolean = false
+
+    if(bool1 && bool2)
+        println("Statement is true - 1")
+
+    if(bool1 && !bool2)
+        println("Statement is true - 2")
+
+    if(bool1 || bool2)
+        println("Statement is true - 3")
+
+    if(!bool1 || bool2)
+        println("Statement is true - 4")
+
+
+}
